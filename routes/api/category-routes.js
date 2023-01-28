fe5ec2aff200d06c98f64e160fa3ddb78917643d
Audrey Alcantara <adrianaquin22@gmail.com>
2023-01-28 14:59:50 -0500

    all category and product routes work tag having issues theory ia models index.js associations

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -42,7 +42,7 @@ router.post('/', async(req, res) => {
 router.put('/:id', async(req, res) => {
   // update a category by its `id` value
   try {
-    const updatedCat = await Category.update({
+    const updatedCat = await Category.update(req.body,{
         where: {
             id: req.params.id
         }
